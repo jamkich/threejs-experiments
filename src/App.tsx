405a907a2dda70ca724239a6a6b6ec18ae7ee373
Jakub <benqumichalik3@gmail.com>
2023-06-13 19:33:38 +0200

    Add arrow

--- src/App.tsx
+++ src/App.tsx
@@ -5,38 +5,36 @@ import WebGL from 'three/addons/capabilities/WebGL.js';
 
 function App() {
   useEffect(() => {
+    // Scene Section
     const scene = new THREE.Scene();
+
+    // Camera Section
     const camera = new THREE.PerspectiveCamera(
-      75,
+      45,
       window.innerWidth / window.innerHeight,
-      0.1,
-      1000
+      1,
+      500
     );
+    camera.position.set(0, 0, 100);
+    camera.lookAt(0, 0, 0);
+
+    // Renderer Section
     const renderer = new THREE.WebGLRenderer({ alpha: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    const material = new THREE.LineBasicMaterial({ color: '#32a866' });
+    const points = [];
+    points.push(new THREE.Vector3(-10, 0, 0));
+    points.push(new THREE.Vector3(0, 10, 0));
+    points.push(new THREE.Vector3(10, 0, 0));
 
-    camera.position.z = 5;
+    const geometry = new THREE.BufferGeometry().setFromPoints(points);
+    const line = new THREE.Line(geometry, material);
 
-    function animate() {
-      requestAnimationFrame(animate);
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-      cube.rotation.z += 0.01;
+    scene.add(line);
 
-      renderer.render(scene, camera);
-    }
-
-    if (WebGL.isWebGLAvailable()) animate();
-    else {
-      const warning = WebGL.getWebGLErrorMessage();
-      throw new Error(`${warning}`);
-    }
+    renderer.render(scene, camera);
   }, []);
 
   return <h1> jamkich's three js experiments</h1>;
